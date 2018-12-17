604937270c4be0c69258de1ec3d2848476f3877e
Ahmed Sabry <ahmedsabry19@outlook.com>
2018-12-17 10:23:31 +0200

    Create AnimationStartPosition.cs

---
+++ Assets/Editor Test/AnimationStartPosition.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationStartPosition : MonoBehaviour
+{
+
+    /// <summary>
+    /// Used for initializing the position from a certian corner.
+    /// </summary>
+    /// <param name="initialPosition">the start position of the transform</param>
+    /// <param name="rect">the rect transform</param>
+    /// <param name="animationFromCornerType">BottomRight, TopRight, BottomLeft, TopLeft, Up, Bottom, Left, Or Right</param>
+    /// <returns></returns>
+    public static Vector3 GetStartPositionFromCorner
+        (Vector3 initialPosition, RectTransform rect, AnimationFromCornerType animationFromCornerType, AnimationFromCornerStartFromType AnimationFromCornerStartFromType)
+    {
+        float startPositionX = 0;
+        float startPositionY = 0;
+
+        if (AnimationFromCornerStartFromType == AnimationFromCornerStartFromType.Screen)
+        {
+            switch (animationFromCornerType)
+            {
+                case (AnimationFromCornerType.BottomRight):
+                    startPositionX = Screen.width + (rect.rect.width / 2);
+                    startPositionY = 0 - (rect.rect.height / 2);
+                    break;
+                case (AnimationFromCornerType.BottomLeft):
+                    startPositionX = 0 - (rect.rect.width / 2);
+                    startPositionY = 0 - (rect.rect.height / 2);
+                    break;
+                case (AnimationFromCornerType.TopRight):
+                    startPositionX = Screen.width + (rect.rect.width / 2);
+                    startPositionY = Screen.height + (rect.rect.height / 2);
+                    break;
+                case (AnimationFromCornerType.TopLeft):
+                    startPositionX = 0 - (rect.rect.width / 2);
+                    startPositionY = Screen.height + (rect.rect.height / 2);
+                    break;
+                case (AnimationFromCornerType.Up):
+                    startPositionX = initialPosition.x;
+                    startPositionY = Screen.height + (rect.rect.height / 2);
+                    break;
+                case (AnimationFromCornerType.Bottom):
+                    startPositionX = initialPosition.x;
+                    startPositionY = 0 - (rect.rect.height / 2);
+                    break;
+                case (AnimationFromCornerType.Left):
+                    startPositionX = 0 - (rect.rect.width / 2);
+                    startPositionY = initialPosition.y;
+                    break;
+                case (AnimationFromCornerType.Right):
+                    startPositionX = Screen.width + (rect.rect.width / 2);
+                    startPositionY = initialPosition.y;
+                    break;
+            }
+        }
+        else if (AnimationFromCornerStartFromType == AnimationFromCornerStartFromType.Screen)
+        {
+            switch (animationFromCornerType)
+            {
+                case (AnimationFromCornerType.BottomRight):
+                    startPositionX = rect.parent.GetComponent<RectTransform>().rect.width + (rect.rect.width / 2);
+                    startPositionY = 0 - (rect.rect.height / 2);
+                    break;
+                case (AnimationFromCornerType.BottomLeft):
+                    startPositionX = 0 - (rect.rect.width / 2);
+                    startPositionY = 0 - (rect.rect.height / 2);
+                    break;
+                case (AnimationFromCornerType.TopRight):
+                    startPositionX = rect.parent.GetComponent<RectTransform>().rect.width + (rect.rect.width / 2);
+                    startPositionY = rect.parent.GetComponent<RectTransform>().rect.height + (rect.rect.height / 2);
+                    break;
+                case (AnimationFromCornerType.TopLeft):
+                    startPositionX = 0 - (rect.rect.width / 2);
+                    startPositionY = rect.parent.GetComponent<RectTransform>().rect.height + (rect.rect.height / 2);
+                    break;
+                case (AnimationFromCornerType.Up):
+                    startPositionX = initialPosition.x;
+                    startPositionY = rect.parent.GetComponent<RectTransform>().rect.height + (rect.rect.height / 2);
+                    break;
+                case (AnimationFromCornerType.Bottom):
+                    startPositionX = initialPosition.x;
+                    startPositionY = 0 - (rect.rect.height / 2);
+                    break;
+                case (AnimationFromCornerType.Left):
+                    startPositionX = 0 - (rect.rect.width / 2);
+                    startPositionY = initialPosition.y;
+                    break;
+                case (AnimationFromCornerType.Right):
+                    startPositionX = rect.parent.GetComponent<RectTransform>().rect.width + (rect.rect.width / 2);
+                    startPositionY = initialPosition.y;
+                    break;
+            }
+        }
+
+        Vector3 startPos = new Vector3(startPositionX, startPositionY, 0);
+
+        return (startPos);
+    }
+}
+
+// Move this enum to AnimationPElement script when you go home.
+public enum AnimationFromCornerStartFromType
+{
+    // Screen is used to start the animation from bordres of the screen.
+    // Rect is used to start the animation from bordres of the current rect.
+    Screen, Rect
+}
